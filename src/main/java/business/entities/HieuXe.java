6f8205420fe179c2bfebc5e2eb6bf17359438b27
Quang Nghia <qnghiauit@hotmail.com>
2017-03-13 23:49:34 +0700

    fix entity class

--- src/main/java/business/entities/HieuXe.java
+++ src/main/java/business/entities/HieuXe.java
@@ -2,12 +2,19 @@ package business.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "hieuxe")
 public class HieuXe {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
 	@Column(name = "MaHieuXe")
 	private String MaHieuXe;
 
--- src/main/java/business/entities/NhomNhaCungCap.java
+++ src/main/java/business/entities/NhomNhaCungCap.java
@@ -2,12 +2,19 @@ package business.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "nhomnhacungcap")
 public class NhomNhaCungCap {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
 	@Column(name = "TenNhom")
 	private String TenNhom;
 
