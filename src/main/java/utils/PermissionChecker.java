083f65be0aa18a5d4fb049a4a32e57715cb62e63
Quang Nghia <qnghiauit@hotmail.com>
2017-06-02 22:38:23 +0700

    Fix PermissionChecker

--- src/main/java/utils/PermissionChecker.java
+++ src/main/java/utils/PermissionChecker.java
@@ -3,7 +3,7 @@ package utils;
 public class PermissionChecker {
 	public static boolean isAdministrator(String permission)
 	{
-		String admin = "Administrator";
+		String admin = "Admin";
 		return permission.toLowerCase().equals(admin.toLowerCase());
 	}
 }
